c8b845ca05d4fe4e43830c87d464d4aefa253646
MaanavD <maanav1@hotmail.com>
2017-10-04 10:42:56 -0400

    Onclick & Modals

--- public/js/script.js
+++ public/js/script.js
@@ -55,3 +55,7 @@ window.onload = function() {
   css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
   document.body.appendChild(css);
 };
+
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').focus()
+})
